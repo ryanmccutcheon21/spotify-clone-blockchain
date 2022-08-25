d14ec3ac3542f0d32dc7bfa9e445e343ab98c897
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-08-25 00:09:00 -0500

    Smart contract setup with Solana

--- utils/const.js
+++ utils/const.js
@@ -1,5 +1,13 @@
 import { PublicKey } from "@solana/web3.js";
+import spotify from './spotify.json'
 
 
 // QuickNode Endpoint that got in email goes here
 export const SOLANA_HOST = 'https://misty-wild-frog.solana-devnet.discover.quiknode.pro/ab4e556afe81d3fd9cdd3e9f8c46977ad32b0bf8/'
+
+
+export const STABLE_POOL_PROGRAM_ID = new PublicKey(
+    "8Niqr1D6m8NL3LDZ9z62vcxcRhBiPu32s2QzDX2qbYXi"
+)
+
+export const STABLE_POOL_IDL = spotify
