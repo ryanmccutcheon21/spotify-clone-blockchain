f1a95e5164bf515263d38e36d096012611d0c554
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-08-27 16:46:13 -0500

    Adding Payment functionality

--- components/Payment.js
+++ components/Payment.js
@@ -1,8 +1,88 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { getProgramInstance } from '../utils/utils'
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { SOLANA_HOST } from '../utils/const'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { PublicKey } from '@solana/web3.js'
+
+const anchor = require('@project-serum/anchor')
+
+const { web3 } = anchor
+
+const { SystemProgram } = web3
+
+const utf8 = anchor.utils.bytes.utf8
+
+const defaultAccounts = {
+    tokenProgram: TOKEN_PROGRAM_ID,
+    clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
+    systemProgram: SystemProgram.programId,
+}
+
+const styles = {
+    main: `w-screen h-screen bg-white text-black flex flex-col justify-center items-center`,
+    button: `bg-[#22C55E] m-3 text-white font-bold py-4 px-7 rounded-full hover:opacity-70 transition`,
+    text: `text-4xl mb-10`,
+    buttons: `flex items-center`
+}
 
 const Payment = () => {
+
+    const wallet = useWallet()
+    const connection = new anchor.web3.Connection(SOLANA_HOST)
+    const program = getProgramInstance(connection, wallet)
+    const [payers, setPayers] = useState([])
+    const [isPaid, setPaid] = useState(false)
+
+    const payClicked = async () => {
+        let [payerSigner] = await anchor.web3.PublicKey.findProgramAddress(
+            [utf8.encode('payer'), wallet.publicKey.toBuffer()],
+            program.programId
+        )
+
+        let payerInfo;
+
+        try {
+            payerInfo = await program.account.payerAccount.fetch(payerSigner)
+        }
+        catch (e) {
+            console.log(e)
+            try {
+                await program.rpc.acceptPayment({
+                    accounts: {
+                        payerWallet: payerSigner,
+                        receiver: new PublicKey(
+                            '0xcdAf7dd5944be5F36edbfe6e3A3327Fce2791E4D'
+                        ),
+                        authority: wallet.publicKey,
+                        ...defaultAccounts,
+                    },
+                })
+                alert('Transaction successful!')
+            } catch (e) {
+                alert(e.message)
+                console.log(e.message)
+            }
+        }
+    }
+
+
     return (
-        <div>Payment</div>
+        <div className={styles.main}>
+            <p className={styles.text}>Make payment</p>
+            <div className={styles.buttons}>
+                <button
+                    className={styles.button}
+                    onClick={payClicked}
+                >
+                    Pay 0.1 Sol
+                </button>
+                <button className={styles.button}>
+                    Verify Payment
+                </button>
+            </div>
+        </div>
     )
 }
 
